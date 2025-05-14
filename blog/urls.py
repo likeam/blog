2424001aa4fe053cae473b903027216dfc2bdff0
Abdul Rehamn <92205472+likeam@users.noreply.github.com>
2025-05-14 15:08:30 +0500

    Building Posts on html

--- blog/urls.py
+++ blog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from posts import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('post/helloworld/', views.helloworld )
+    path('post/', include('posts.urls')),
+    path('<int:id>/', views.google)
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,83 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
-def helloworld(request):
-    return HttpResponse("Hello World")
+posts = [
+    
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  },
+  {
+    "userId": 1,
+    "id": 5,
+    "title": "nesciunt quas odio",
+    "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+  },
+  {
+    "userId": 1,
+    "id": 6,
+    "title": "dolorem eum magni eos aperiam quia",
+    "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+  },
+  {
+    "userId": 1,
+    "id": 7,
+    "title": "magnam facilis autem",
+    "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+  }  
+
+]
+
+def home(request):
+  
+    html = ""
+    for post in posts:
+        html += f'''
+        <div>
+        <a href="/post/{post['id']}">
+            <h1>{post['id']} - {post['title']}</h1></a>
+            <p>{post['body']}</p>
+        </div>
+        '''
+    return render(request, 'posts/home.html', {"posts":posts})
+
+def post(request, id):
+    vali_id = False
+    for post in posts: 
+        if post['id'] == id:
+            post_dict = post
+            vali_id = True
+            break
+    if vali_id:
+        html = f''' 
+            <h1>{post_dict['title']}</h1>
+            <P>{post_dict['body']}</p>
+        '''
+        return HttpResponse(html)
+    else:
+        return  HttpResponseNotFound("Post Not Available")
+    
+def google(request, id):
+    url = reverse('post', args=[id])
+    return HttpResponseRedirect(url)
